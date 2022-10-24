dc0a53e757d619ebecc35f0d1277d8c9189811d1
Matheus <matheusnicolas043@gmail.com>
2022-10-23 23:39:20 -0300

    icone hamburguer e coisinhas

--- js.js
+++ js.js
@@ -7,10 +7,12 @@ function abrir() {
     if(teste == true){
         menuzinho.style.height='100px'
         menuzinho.style.transition='0.5s'
+        hamb.src = "img/closeicon.png"
         teste = false
     }else{
         menuzinho.style.height='0px'
         menuzinho.style.transition='0.5s'
+        hamb.src = "img/menuhamb.svg"
         teste = true
     }
     
